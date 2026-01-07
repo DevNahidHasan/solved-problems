ce12384850d6b566a0a66b4d233044232c067c84
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-07 22:26:35 +0600

    Solved Leetcode 9 - Palindrome Number

---
+++ 02. Leetcode problems from Striver's A2Z DSA Sheet/a_Learn-the-basics/src/d_Know_Basic_Maths/b_L9_PallindromeNumber.java
@@ -0,0 +1,34 @@
+package d_Know_Basic_Maths;
+
+public class b_L9_PallindromeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(10));
+    }
+    public static boolean isPalindrome(int x) {
+        return x < 0 || (x % 10 == 0 && x != 0)? false : x == reverse(x);
+    }
+
+    public static int reverse(int x) {
+        int MAX = Integer.MAX_VALUE;
+        int MIN = Integer.MIN_VALUE;
+        int rev = 0;
+
+        while(x != 0){
+
+            int lastDigit = x % 10;
+
+            if(rev > MAX / 10 || (rev == MIN / 100 && lastDigit > 7)){
+                return 0;
+            }
+            if(rev < MIN/10 || (rev == MIN / 10 && lastDigit > 8)){
+                return 0;
+            }
+
+            rev = rev * 10 + lastDigit;
+
+            x = x / 10;
+        }
+
+        return rev;
+    }
+}
