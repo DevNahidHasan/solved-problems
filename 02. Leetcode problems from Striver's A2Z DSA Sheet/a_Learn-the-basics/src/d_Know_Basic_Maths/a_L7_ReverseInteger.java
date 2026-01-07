6fd6bf4582245ff515f1138c2167edebe63e0ccc
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-07 22:05:27 +0600

    New folder created and Solved Leetcode 7 - Reverse Integer, a_L7_ReverseInteger

---
+++ 02. Leetcode problems from Striver's A2Z DSA Sheet/a_Learn-the-basics/src/d_Know_Basic_Maths/a_L7_ReverseInteger.java
@@ -0,0 +1,31 @@
+package d_Know_Basic_Maths;
+
+public class a_L7_ReverseInteger {
+    public static void main(String[] args) {
+        System.out.println(reverse(-123));
+    }
+    public static int reverse(int x) {
+        int MAX = Integer.MAX_VALUE;
+        int MIN = Integer.MIN_VALUE;
+        int rev = 0;
+
+        while(x != 0){
+
+            int lastDigit = x % 10;
+
+            if(rev > MAX / 10 || (rev == MIN / 100 && lastDigit > 7)){
+                return 0;
+            }
+            if(rev < MIN/10 || (rev == MIN / 10 && lastDigit > 8)){
+                return 0;
+            }
+
+            rev = rev * 10 + lastDigit;
+
+            x = x / 10;
+        }
+
+        return rev;
+    }
+
+}
