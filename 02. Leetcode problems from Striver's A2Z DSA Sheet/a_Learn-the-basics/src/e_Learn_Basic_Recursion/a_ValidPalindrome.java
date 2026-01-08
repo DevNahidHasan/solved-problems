093dedc36de62102cf167fe62c407f8aff6bf97a
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-08 23:25:30 +0600

    Solved Leetcode 125 - Valid Palindrome. a_L125_ValidPalindrome

---
+++ 02. Leetcode problems from Striver's A2Z DSA Sheet/a_Learn-the-basics/src/e_Learn_Basic_Recursion/a_ValidPalindrome.java
@@ -0,0 +1,26 @@
+package e_Learn_Basic_Recursion;
+
+public class a_ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(""));
+    }
+
+    public  static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "");
+        System.out.println(s);
+        return checkPalindrome(s, 0, s.length() - 1 );
+    }
+
+    static boolean checkPalindrome(String str , int s, int e){
+        if(s > e){
+            return true;
+        }
+
+        if(str.charAt(s) == str.charAt(e)){
+            return checkPalindrome(str,s+1,e-1);
+        }else{
+            return false;
+        }
+    }
+}
