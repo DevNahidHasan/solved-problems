ded9e53887bbae869470a2bec2e56041cd13bea8
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-18 15:42:28 +0600

    Solved Leetcode 26 - Remove duplicates

---
+++ 01. Leetcode/Arrays/src/a_easy/L26_RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package a_easy;
+
+public class L26_RemoveDuplicates {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{1,1,2}));
+    }
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 1;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i - 1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
