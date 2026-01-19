b01889a0f63a18784eb3d4438a95add7788ced9e
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-19 11:21:59 +0600

    Solved Leetcode 485 - Max Consecutive Ones, L485_MaxConsecutiveOnes

---
+++ 01. Leetcode/Arrays/src/a_easy/L485_MaxConsecutiveOnes.java
@@ -0,0 +1,24 @@
+package a_easy;
+
+public class L485_MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        System.out.println(findMaxConsecutiveOnes(new int[]{1,0,1,1,0,1}));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == 0){
+                if (count > max){
+                    max = count;
+                }
+                count = 0;
+            }
+            else{
+                count += 1;
+            }
+        }
+        return max > count ? max : count;
+    }
+}
