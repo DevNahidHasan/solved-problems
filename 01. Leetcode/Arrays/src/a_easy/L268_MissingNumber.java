0b7918bab3eed88d6bba892315fe1332b95fd881
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-21 23:10:49 +0600

    Used XOR to solve Leetcode 268 - Missing Numbers, L268_MissingNumbers

--- 01. Leetcode/Arrays/src/a_easy/L268_MissingNumber.java
+++ 01. Leetcode/Arrays/src/a_easy/L268_MissingNumber.java
@@ -3,6 +3,7 @@ package a_easy;
 public class L268_MissingNumber {
     public static void main(String[] args) {
         System.out.println(missingNumber(new int[]{9,6,4,2,3,5,7,0,1}));
+        System.out.println(missingNumber2(new int[]{9,6,4,2,3,5,7,0,1}));
     }
     public static int missingNumber(int[] nums) {
         int sum = nums.length * ( nums.length + 1) / 2;
@@ -10,5 +11,13 @@ public class L268_MissingNumber {
             sum -= nums[i];
         }
         return sum;
+    }
+    public static int missingNumber2(int[] nums) {
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= i ^ nums[i];
+        }
+        return xor ^ nums.length;
+
     }
 }
