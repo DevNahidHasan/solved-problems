e98c891c0c34f101d471c7b85e0452c4bd90bbb9
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-22 08:10:17 +0600

    Another optimal solution using XOR for Leetcode 268 - Missing Number, L268_MissingNumber

--- 01. Leetcode/Arrays/src/a_easy/L268_MissingNumber.java
+++ 01. Leetcode/Arrays/src/a_easy/L268_MissingNumber.java
@@ -4,6 +4,7 @@ public class L268_MissingNumber {
     public static void main(String[] args) {
         System.out.println(missingNumber(new int[]{9,6,4,2,3,5,7,0,1}));
         System.out.println(missingNumber2(new int[]{9,6,4,2,3,5,7,0,1}));
+        System.out.println(missingNumber3(new int[]{9,6,4,2,3,5,7,0,1}));
     }
     public static int missingNumber(int[] nums) {
         int sum = nums.length * ( nums.length + 1) / 2;
@@ -18,6 +19,19 @@ public class L268_MissingNumber {
             xor ^= i ^ nums[i];
         }
         return xor ^ nums.length;
+
+    }
+    public static int missingNumber3(int[] nums) {
+        int xor1 = 0, xor2 = 0;
+
+        // Calculate XOR of all array elements
+        for (int i = 0; i < nums.length; i++) {
+            xor1 = xor1 ^ (i + 1); // XOR up to [1...N]
+            xor2 = xor2 ^ nums[i]; // XOR of array elements
+        }
+
+        // XOR of xor1 and xor2 gives missing number
+        return (xor1 ^ xor2);
 
     }
 }
