d7dc0f7a8938b580d25e738dc11bec460bcb77a2
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-25 18:37:55 +0600

    Brute force solution added of problem Leetcode 283 - Move Zeros

--- 01. Leetcode/Arrays/src/a_easy/L283_MoveZeros.java
+++ 01. Leetcode/Arrays/src/a_easy/L283_MoveZeros.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class L283_MoveZeros {
     public static void main(String[] args) {
         int[] nums = {0,1,0,3,12};
-        moveZeroes(nums);
+        //moveZeroes(nums);
+        moveZeroes2(nums);
         System.out.println(Arrays.toString(nums));
     }
     public static void moveZeroes(int[] nums) {
@@ -18,6 +19,31 @@ public class L283_MoveZeros {
 
                 j++;
             }
+        }
+    }
+    //brute force approach
+    public static void moveZeroes2(int[] nums) {
+        int n = nums.length;
+
+        // Create a temporary array to store non-zero elements
+        int[] temp = new int[n];
+        int count = 0;
+
+        // Copy non-zero elements to temp
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                temp[count++] = nums[i];
+            }
+        }
+
+        // Copy non-zero elements back to nums
+        for (int i = 0; i < count; i++) {
+            nums[i] = temp[i];
+        }
+
+        // Fill the rest with zeroes
+        for (int i = count; i < n; i++) {
+            nums[i] = 0;
         }
     }
 }
