1f38b7eab31ed1a63635bdcb2ec6e1030ffae840
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-30 21:08:51 +0600

    Better solution of Leetcode 1 - Two Sum added

--- 01. Leetcode/Arrays/src/b_medium/L1_TwoSum.java
+++ 01. Leetcode/Arrays/src/b_medium/L1_TwoSum.java
@@ -1,14 +1,19 @@
 package b_medium;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class L1_TwoSum {
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] result = twoSum(nums, target);
+        int[] result2 = twoSum2(nums, target);
         System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(result2));
     }
+    //brute force approach
     public static int[] twoSum(int[] nums, int target) {
 
         int n = nums.length;
@@ -31,5 +36,37 @@ public class L1_TwoSum {
         // Return {-1, -1} if no such pair is found
         return new int[]{-1, -1};
 
+    }
+
+    //better approach
+    public static int[] twoSum2(int[] nums, int target) {
+
+
+        // Map to store (element, index) pairs
+        Map<Integer, Integer> mpp = new HashMap<>();
+
+        // Size of the nums array
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            // Current number in the array
+            int num = nums[i];
+
+            // Number needed to reach the target
+            int moreNeeded = target - num;
+
+            // Check if the complement exists in map
+            if (mpp.containsKey(moreNeeded)) {
+                /* Return the indices of the two
+                numbers that sum up to target*/
+                return new int[]{mpp.get(moreNeeded), i};
+            }
+
+            // Store current number and its index in map
+            mpp.put(nums[i], i);
+        }
+
+        // If no such pair found, return {-1, -1}
+        return new int[]{-1, -1};
     }
 }
