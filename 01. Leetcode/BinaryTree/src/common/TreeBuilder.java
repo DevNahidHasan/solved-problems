480a83f63fab1b283c860f7d0343b089542dca45
DevNahidHasan <smnahid.hsn@gmail.com>
2026-02-08 17:51:24 +0600

    Common java classes to build a tree are added

---
+++ 01. Leetcode/BinaryTree/src/common/TreeBuilder.java
@@ -0,0 +1,33 @@
+package common;
+
+import java.util.*;
+
+public class TreeBuilder {
+    public static TreeNode buildTree(Integer[] arr) {
+        if (arr == null || arr.length == 0 || arr[0] == null) return null;
+
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+        while (!queue.isEmpty() && i < arr.length) {
+            TreeNode current = queue.poll();
+
+            // Left child
+            if (i < arr.length && arr[i] != null) {
+                current.left = new TreeNode(arr[i]);
+                queue.add(current.left);
+            }
+            i++;
+
+            // Right child
+            if (i < arr.length && arr[i] != null) {
+                current.right = new TreeNode(arr[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+        return root;
+    }
+}
